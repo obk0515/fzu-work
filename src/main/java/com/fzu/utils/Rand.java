bb969adcfc6aa32907c7b0000faceb64541b08bb
Stellaxinx <1608322198@qq.com>
2022-10-10 18:35:59 +0800

    v2

--- src/main/java/com/fzu/utils/Rand.java
+++ src/main/java/com/fzu/utils/Rand.java
@@ -21,36 +21,59 @@ public class Rand {
 	//固定缺勤的学生数
 	public int fixed_absent_num;
 
-	public Rand() {
-
-	}
-
 	//获取一门课固定会缺勤的学生数量（生成5-8中的一个数字）
 	public int get_fixed_absent_num() {
 		Random random = new Random();
 		int fixed_absent_num=5 + random.nextInt( 4);
 		return fixed_absent_num;
 	}
+	
+	public static int random(List<Integer> weight){
+		List<Integer> weightTmp = new ArrayList<Integer>();
+		weightTmp.add(0);
+		Integer sum = 0;
+		for( Integer d : weight ){
+			sum += d;
+			weightTmp.add(sum);
+		}
+		Random random = new Random();
+		int rand = random.nextInt(sum);
+		int index = 0;
+		for(int i = weightTmp.size()-1; i >0; i--){
+			if( rand >= weightTmp.get(i)){
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
 
 
-	//固定缺勤同学以及他们的缺的课次
+	//生成固定缺勤同学以及他们的缺的课次
 	public Map<Integer,List<Integer>> function1(){
 		this.setFixed_absent_num(this.get_fixed_absent_num());
 		//创建hashmap
 		Map<Integer,List<Integer>> map=new HashMap<Integer,List<Integer>>();
 		//创建随机数对象
 		Random r = new Random();
-		//random固定缺勤的学生id，不重复
-		//index用来做索引值用于数组的迭代
-		int index = 0;
+		//分配权重
+		List<Integer> weight=new ArrayList<Integer>();
+		for(int i=1;i<=90;i++){
+			if(i<=50) weight.add(1);
+			if(i>50&&i<=60) weight.add(5);
+			if(i>60&&i<=70) weight.add(10);
+			if(i>70) weight.add(40);
+		}
+		//按权重生成固定缺勤的学生id，不重复
+		int index = 0;//index用来做索引值用于数组的迭代
 		while(index<fixed_absent_num){
 
 			//从1-90中随机生成
-			int id=1 + r.nextInt( 90);
+			int id=random(weight);
 			for(int i=0;i<index;i++) {
 				if(id==absent_stu_id.get(i)) {
 					//如果生成的随机数已经存在，重新生成
-					id=1 + r.nextInt( 90);
+					id=random(weight);
 					i=-1;
 				}
 			}
@@ -84,6 +107,14 @@ public class Rand {
 		Map<Integer,List<Integer>> map=new HashMap<Integer,List<Integer>>();
 		//创建随机数对象
 		Random r = new Random();
+		//分配权重
+		List<Integer> weight=new ArrayList<Integer>();
+		for(int i=1;i<=90;i++){
+			if(i<=50) weight.add(1);
+			if(i>50&&i<=60) weight.add(5);
+			if(i>60&&i<=70) weight.add(10);
+			if(i>70) weight.add(40);
+		}
 		for(int j=1;j<21;j++) {
 			//创建排序集合
 			Set<Integer> set = new TreeSet<Integer>();
@@ -91,11 +122,11 @@ public class Rand {
 			int absent_num= r.nextInt(4);
 			while (set.size()<absent_num) {
 				//产生一个随机数，添加到集合
-				int number = r.nextInt(90) + 1;
+				int number = random(weight);
 				for(int i=0;i<fixed_absent_num;i++) {
 					if(number==absent_stu_id.get(i)) {
 						//如果生成的学生id是固定缺课的学生，重新生成
-						number = r.nextInt(90) + 1;
+						number = random(weight);
 						i=-1;
 					}
 				}
